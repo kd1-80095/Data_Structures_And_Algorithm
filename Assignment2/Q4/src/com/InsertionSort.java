e3aab1dcdae1b97e408501349f98ade3e0a3ca4b
kd1-80095 <aniketjadhav241299@.com>
2023-11-04 15:37:16 +0530

    Assignment2_Uploaded

--- Assignment2/Q4/src/com/InsertionSort.java
+++ Assignment2/Q4/src/com/InsertionSort.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class InsertionSort {
 	public static int insertionSort(int arr[],int N)
 	{
-		int cnt = 0;
-		for(int i=1;i<N;i++)
+	int cnt = 0;
+	for(int i=1;i<N;i++)
+	{
+		int temp = arr[i];
+		int j = i-1;
+		cnt++;
+		while(j>=0 && arr[j]<temp)
 		{
-			int temp = arr[i];
-			int j = i-1;
-			cnt++;
-			while(j>=0 && arr[j]<temp)
-			{
-				arr[j+1]=arr[j];
-				j--;
+			arr[j+1]=arr[j];
+			j--;
 			}
 			arr[j+1]=temp;
 		}
